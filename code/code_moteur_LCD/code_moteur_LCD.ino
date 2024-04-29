b7a5b79f83c6db288aa95148fe08a6e912fb12d5
charlilou <charlilou.maes@gmail.com>
2024-04-29 15:50:21 +0200

    Add files via upload
    Code pour ejecter dès qu'un objet est détecter avec controle de la direction du tapis

--- code/code_moteur_LCD/code_moteur_LCD.ino
+++ code/code_moteur_LCD/code_moteur_LCD.ino
@@ -1,33 +1,68 @@
+#include <Arduino.h>
 
+//LCD
 #include <LiquidCrystal.h>
-#define stepPin 3
-#define dirPin 4
-int pin_pot = A1;
 const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+//StepMotor
+#include "A4988.h"
+int RPM = 120;  
+int DIR = 5;
+int STEP = 6;
+int servo_pin = 4;
+int IRPin = 3;
+int MOTOR_STEPS_P = 400;  //1.8 ° per steps
+A4988 stepper(MOTOR_STEPS_P, DIR, STEP);
+//IR
+#include <Servo.h>
+const int sensorPin = A2;  // Pin connected to sensor
+int sensorVal;
+//servo
+Servo eject;
+
+int value;
+int detect;
+
 
 void setup() {
-  // Sets the two pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  Serial.begin(9600);  // ouvre le port série
-  pinMode(pin_pot, INPUT);
+  pinMode(sensorPin, INPUT);
+  stepper.begin(RPM);
+  stepper.enable();
+  eject.attach(servo_pin);
+  pinMode(IRPin, INPUT);
+  Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("Vitesse moteur");
-  
+
 }
 void loop() {
-  int pot = analogRead(pin_pot);
-  int tempo = (pot / 400) +1;
-  digitalWrite(dirPin, LOW);  // Enables the motor to move in a particular direction
-  // Makes 200 pulses for making one full cycle rotation
-  for (int x = 0; x < 800; x++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(700);  // by changing this time delay between the steps we can change the rotation speed
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(pot+45);// le delais ne peux pas être<45 sinon moteur bug
+  sensorVal = analogRead(sensorPin);
+  // Si le capteur infrarouge est activé
+  Serial.println(digitalRead(IRPin));
+
+  if (sensorVal < 200) {
+    stepper.move(100);
+    detection();
   }
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(4-tempo);
+  if (sensorVal > 800) {
+    stepper.move(-100);
+    detection();
+  }
+  //un tour
+  delay(1);
+}
+
+
+void detection() {
+
+  if (digitalRead(IRPin) == LOW) {
+    // Déplacer le servomoteur à 180 degrés
+    lcd.print("hello, world!");
+    eject.write(180);
+     // Attendre 1 seconde
+    // Ramener le servomoteur à 0 degré
+    stepper.move(800);
+
+    eject.write(90);
+
+  }
 }
